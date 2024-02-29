292d98a4a39b12700c40b80801fe3adbc3b2c834
Averill Cate, Jr <acatejr@gmail.com>
2024-02-29 13:40:49 -0700

    Left off at 45:22

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import Accordian from './components/accordian'
+// import Accordian from './components/accordian'
+import RandomColor from './components/random-color'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <Accordian />
+      {/* <Accordian /> */}
+      <RandomColor />
     </div>
   )
 }
